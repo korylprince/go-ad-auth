49a1895cb55233b43e8424cab6bb5c3f5c5a1808
Kory Prince <korylprince@gmail.com>
2014-11-25 19:19:49 -0600

    made getDomain more robust

--- auth_test.go
+++ auth_test.go
@@ -3,10 +3,27 @@ package auth
 import "testing"
 
 func TestGetDomain(t *testing.T) {
-    tests := []string{"dc=example,dc=com", "ou=test,dc=example,dc=com", "dc=example, dc=com"}
-    for _, test := range tests {
-        if getDomain(test) != "example.com" {
-            t.Error("Failed Test:", test, "\n\tOutput:", getDomain(test))
-        }
-    }
+	tests := []string{"dc=example,dc=com",
+		"ou=test,dc=example,dc=com",
+		"dc=example, dc=com",
+		"DC=example,DC=com",
+		"OU=test,dc=example,DC=com"}
+	for _, test := range tests {
+		if domain, err := getDomain(test); domain != "example.com" {
+			if err != nil {
+				t.Error("Failed Test:", test, "\n\tError:", err)
+			} else {
+				t.Error("Failed Test:", test, "\n\tOutput:", domain)
+			}
+		}
+	}
+	error_tests := []string{"",
+		"com",
+		"ou=test",
+		"OU=test"}
+	for _, test := range error_tests {
+		if _, err := getDomain(test); err == nil {
+			t.Error("Failed Test:", test, "\n\tError: err not nil")
+		}
+	}
 }
